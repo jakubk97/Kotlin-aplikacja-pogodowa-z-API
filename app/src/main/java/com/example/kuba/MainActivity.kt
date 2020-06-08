31dc3fdc40a6e2d410f19778944ad8ece684d3d2
jakubk97 <jkuba-kar@o2.pl>
2020-06-08 21:12:25 +0200

    rozszerzenie
    rozszerzenie o wyszukiwane po kodzie pocztowym(USA) , koordynatach i id miasta

--- app/src/main/java/com/example/kuba/MainActivity.kt
+++ app/src/main/java/com/example/kuba/MainActivity.kt
@@ -29,18 +29,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_search.setOnClickListener {
-            if (edit_search.text.toString().isNotEmpty()) {
-                beginSearch(edit_search.text.toString())
+        btn_search_miejscowosc.setOnClickListener {
+            if (edit_search_miejscowosc.text.toString().isNotEmpty()) {
+                beginSearchCity(edit_search_miejscowosc.text.toString())
                 val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(btn_search.windowToken, 0)
+                imm.hideSoftInputFromWindow(btn_search_miejscowosc.windowToken, 0)
+            }
+        }
+
+        btn_search_id.setOnClickListener {
+            if (edit_search_id.text.toString().isNotEmpty()) {
+                beginSearchId(edit_search_id.text.toString())
+                val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(btn_search_id.windowToken, 0)
+            }
+        }
+
+        btn_search_koordynaty.setOnClickListener {
+            if (edit_search_koordynaty_dlugosc.text.toString().isNotEmpty() && edit_search_koordynaty_szerokosc.text.toString().isNotEmpty()) {
+                beginSearchCoords(edit_search_koordynaty_dlugosc.text.toString(),edit_search_koordynaty_szerokosc.text.toString())
+                val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(btn_search_koordynaty.windowToken, 0)
+            }
+        }
+
+        btn_search_kod_pocztowy.setOnClickListener {
+            if (edit_search_kod_pocztowy.text.toString().isNotEmpty()) {
+                beginSearchZIP(edit_search_kod_pocztowy.text.toString())
+                val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(btn_search_kod_pocztowy.windowToken, 0)
             }
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun beginSearch(searchString: String) {
-        disposable = wikiApiServe.hitCountCheck(
+    private fun beginSearchZIP(searchString: String) {
+        disposable = wikiApiServe.hitZIP(
             searchString,
             "b9a31dcb2d2b84843ea2eba6b48ff5f9",
             "metric",
@@ -75,6 +99,117 @@ class MainActivity : AppCompatActivity() {
             )
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun beginSearchCoords(searchStringSzerokosc: String,searchStringDlugosc:String) {
+        disposable = wikiApiServe.hitCoords(
+            searchStringSzerokosc,
+            searchStringDlugosc,
+            "b9a31dcb2d2b84843ea2eba6b48ff5f9",
+            "metric",
+            "pl"
+        )
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result ->
+                    city.text = "${result.name}"
+                    temp.text = "${result.main.temp} °C"
+                    tempDes.text = "Temperatura"
+                    pressure.text = "${result.main.pressure} hPa"
+                    pressureDes.text = "Ciśnienie"
+                    description.text = "${result.weather[0].description}"
+//                    date.text = "${java.time.format.DateTimeFormatter.ISO_INSTANT.format(java.time.Instant.ofEpochSecond(result.dt.toLong()))}"
+                    date.text = "${getDate(result.dt.toLong() * 1000, "dd MM yyyy")}"
+                    if (result.weather[0].main == "Clear") {
+                        icon.setBackgroundResource(R.drawable.ic_wb_sunny_black_24dp)
+                    } else if (result.weather[0].main == "Rainy") {
+                        icon.setBackgroundResource(R.drawable.ic_rain)
+                    } else if (result.weather[0].main == "Windy") {
+                        icon.setBackgroundResource(R.drawable.ic_icons8_winter_50)
+                    } else if (result.weather[0].main == "Clouds") {
+                        icon.setBackgroundResource(R.drawable.ic_clouds)
+                    }
+                    sunrise.text = "Wschód słońca: ${getTime(result.sys.sunrise.toLong() * 1000)}"
+                    sunset.text = "Zachód słońca: ${getTime(result.sys.sunset.toLong() * 1000)}"
+                }
+                ,
+                { error -> Toast.makeText(this, "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych", Toast.LENGTH_LONG).show() } //error.message
+            )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun beginSearchId(searchString: String) {
+        disposable = wikiApiServe.hitId(
+            searchString,
+            "b9a31dcb2d2b84843ea2eba6b48ff5f9",
+            "metric",
+            "pl"
+        )
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result ->
+                    city.text = "${result.name}"
+                    temp.text = "${result.main.temp} °C"
+                    tempDes.text = "Temperatura"
+                    pressure.text = "${result.main.pressure} hPa"
+                    pressureDes.text = "Ciśnienie"
+                    description.text = "${result.weather[0].description}"
+//                    date.text = "${java.time.format.DateTimeFormatter.ISO_INSTANT.format(java.time.Instant.ofEpochSecond(result.dt.toLong()))}"
+                    date.text = "${getDate(result.dt.toLong() * 1000, "dd MM yyyy")}"
+                    if (result.weather[0].main == "Clear") {
+                        icon.setBackgroundResource(R.drawable.ic_wb_sunny_black_24dp)
+                    } else if (result.weather[0].main == "Rainy") {
+                        icon.setBackgroundResource(R.drawable.ic_rain)
+                    } else if (result.weather[0].main == "Windy") {
+                        icon.setBackgroundResource(R.drawable.ic_icons8_winter_50)
+                    } else if (result.weather[0].main == "Clouds") {
+                        icon.setBackgroundResource(R.drawable.ic_clouds)
+                    }
+                    sunrise.text = "Wschód słońca: ${getTime(result.sys.sunrise.toLong() * 1000)}"
+                    sunset.text = "Zachód słońca: ${getTime(result.sys.sunset.toLong() * 1000)}"
+                }
+                ,
+                { error -> Toast.makeText(this, "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych", Toast.LENGTH_LONG).show() } //error.message
+            )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun beginSearchCity(searchString: String) {
+        disposable = wikiApiServe.hitCity(
+            searchString,
+            "b9a31dcb2d2b84843ea2eba6b48ff5f9",
+            "metric",
+            "pl"
+        )
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result ->
+                    city.text = "${result.name}"
+                    temp.text = "${result.main.temp} °C"
+                    tempDes.text = "Temperatura"
+                    pressure.text = "${result.main.pressure} hPa"
+                    pressureDes.text = "Ciśnienie"
+                    description.text = "${result.weather[0].description}"
+//                    date.text = "${java.time.format.DateTimeFormatter.ISO_INSTANT.format(java.time.Instant.ofEpochSecond(result.dt.toLong()))}"
+                    date.text = "${getDate(result.dt.toLong() * 1000, "dd MM yyyy")}"
+                    if (result.weather[0].main == "Clear") {
+                        icon.setBackgroundResource(R.drawable.ic_wb_sunny_black_24dp)
+                    } else if (result.weather[0].main == "Rainy") {
+                        icon.setBackgroundResource(R.drawable.ic_rain)
+                    } else if (result.weather[0].main == "Windy") {
+                        icon.setBackgroundResource(R.drawable.ic_icons8_winter_50)
+                    } else if (result.weather[0].main == "Clouds") {
+                        icon.setBackgroundResource(R.drawable.ic_clouds)
+                    }
+                    sunrise.text = "Wschód słońca: ${getTime(result.sys.sunrise.toLong() * 1000)}"
+                    sunset.text = "Zachód słońca: ${getTime(result.sys.sunset.toLong() * 1000)}"
+                }
+                ,
+                { error -> Toast.makeText(this, "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych", Toast.LENGTH_LONG).show() } //error.message
+            )
+    }
     override fun onPause() {
         super.onPause()
         disposable?.dispose()
--- app/src/main/java/com/example/kuba/WeatherApiService.kt
+++ app/src/main/java/com/example/kuba/WeatherApiService.kt
@@ -10,10 +10,29 @@ import retrofit2.http.Query
 interface WeatherApiService {
 
     @GET("weather?")
-    fun hitCountCheck(@Query("q") city: String,
+    fun hitCity(@Query("q") city: String,
                       @Query("appid") appid: String,
                       @Query("units") units: String,
                       @Query("lang") lang: String): Observable<Model.Result>
+    @GET("weather?")
+    fun hitId(@Query("id") id: String,
+                @Query("appid") appid: String,
+                @Query("units") units: String,
+                @Query("lang") lang: String): Observable<Model.Result>
+
+    @GET("weather?")
+    fun hitCoords(@Query("lat") lat: String,
+                  @Query("lon") lon: String,
+                @Query("appid") appid: String,
+                @Query("units") units: String,
+                @Query("lang") lang: String): Observable<Model.Result>
+
+    @GET("weather?")
+    fun hitZIP(@Query("zip") zip: String,
+                @Query("appid") appid: String,
+                @Query("units") units: String,
+                @Query("lang") lang: String): Observable<Model.Result>
+
 
     companion object {
         fun create(): WeatherApiService {

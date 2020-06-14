f50eeb785e6124bf1422e271d3c6aa3b3b3d761e
agnikuz262 <aga.kuznia@interia.pl>
2020-06-14 21:33:50 +0200

    Create new design

--- app/src/main/java/com/example/kuba/MainActivity.kt
+++ app/src/main/java/com/example/kuba/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.kuba
 
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kuba.ui.WeatherDetails
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.weather_fragment.*
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -47,7 +48,10 @@ class MainActivity : AppCompatActivity() {
 
         btn_search_koordynaty.setOnClickListener {
             if (edit_search_koordynaty_dlugosc.text.toString().isNotEmpty() && edit_search_koordynaty_szerokosc.text.toString().isNotEmpty()) {
-                beginSearchCoords(edit_search_koordynaty_dlugosc.text.toString(),edit_search_koordynaty_szerokosc.text.toString())
+                beginSearchCoords(
+                    edit_search_koordynaty_dlugosc.text.toString(),
+                    edit_search_koordynaty_szerokosc.text.toString()
+                )
                 val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(btn_search_koordynaty.windowToken, 0)
             }
@@ -74,14 +78,14 @@ class MainActivity : AppCompatActivity() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { result ->
-                    city.text = "${result.name}"
-                    temp.text = "${result.main.temp} °C"
+                    weather_city.text = "${result.name}"
+                    weather_temp.text = "${result.main.temp.toInt()} °C"
                     tempDes.text = "Temperatura"
-                    pressure.text = "${result.main.pressure} hPa"
+                    weather_pressure.text = "${result.main.pressure.toInt()} hPa"
                     pressureDes.text = "Ciśnienie"
-                    description.text = "${result.weather[0].description}"
+                    weather_description.text = "${result.weather[0].description}"
 //                    date.text = "${java.time.format.DateTimeFormatter.ISO_INSTANT.format(java.time.Instant.ofEpochSecond(result.dt.toLong()))}"
-                    date.text = "${getDate(result.dt.toLong() * 1000, "dd MM yyyy")}"
+                    weather_date.text = "${getDate(result.dt.toLong() * 1000, "dd MM yyyy")}"
                     if (result.weather[0].main == "Clear") {
                         icon.setBackgroundResource(R.drawable.ic_wb_sunny_black_24dp)
                     } else if (result.weather[0].main == "Rainy") {
@@ -93,14 +97,22 @@ class MainActivity : AppCompatActivity() {
                     }
                     sunrise.text = "Wschód słońca: ${getTime(result.sys.sunrise.toLong() * 1000)}"
                     sunset.text = "Zachód słońca: ${getTime(result.sys.sunset.toLong() * 1000)}"
-                }
-                ,
-                { error -> Toast.makeText(this, "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych", Toast.LENGTH_LONG).show() } //error.message
+
+                    val intent = Intent(this, WeatherDetails::class.java).apply {}
+                    this.startActivity(intent)
+                },
+                { error ->
+                    Toast.makeText(
+                        this,
+                        "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } //error.message
             )
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun beginSearchCoords(searchStringSzerokosc: String,searchStringDlugosc:String) {
+    private fun beginSearchCoords(searchStringSzerokosc: String, searchStringDlugosc: String) {
         disposable = wikiApiServe.hitCoords(
             searchStringSzerokosc,
             searchStringDlugosc,
@@ -133,7 +145,13 @@ class MainActivity : AppCompatActivity() {
                     sunset.text = "Zachód słońca: ${getTime(result.sys.sunset.toLong() * 1000)}"
                 }
                 ,
-                { error -> Toast.makeText(this, "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych", Toast.LENGTH_LONG).show() } //error.message
+                { error ->
+                    Toast.makeText(
+                        this,
+                        "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } //error.message
             )
     }
 
@@ -170,7 +188,13 @@ class MainActivity : AppCompatActivity() {
                     sunset.text = "Zachód słońca: ${getTime(result.sys.sunset.toLong() * 1000)}"
                 }
                 ,
-                { error -> Toast.makeText(this, "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych", Toast.LENGTH_LONG).show() } //error.message
+                { error ->
+                    Toast.makeText(
+                        this,
+                        "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } //error.message
             )
     }
 
@@ -186,30 +210,42 @@ class MainActivity : AppCompatActivity() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { result ->
-                    city.text = "${result.name}"
-                    temp.text = "${result.main.temp} °C"
+                    weather_city.text = "${result.name}"
+                    weather_temp.text = "${result.main.temp.toInt()} °C"
                     tempDes.text = "Temperatura"
-                    pressure.text = "${result.main.pressure} hPa"
+                    weather_pressure.text = "${result.main.pressure.toInt()} hPa"
                     pressureDes.text = "Ciśnienie"
-                    description.text = "${result.weather[0].description}"
+                    weather_description.text = "${result.weather[0].description}"
 //                    date.text = "${java.time.format.DateTimeFormatter.ISO_INSTANT.format(java.time.Instant.ofEpochSecond(result.dt.toLong()))}"
-                    date.text = "${getDate(result.dt.toLong() * 1000, "dd MM yyyy")}"
+                    weather_date.text = "${getDate(result.dt.toLong() * 1000, "dd MM yyyy")}"
                     if (result.weather[0].main == "Clear") {
-                        icon.setBackgroundResource(R.drawable.ic_wb_sunny_black_24dp)
+                        weather_icon.setBackgroundResource(R.drawable.ic_wb_sunny_black_24dp)
                     } else if (result.weather[0].main == "Rainy") {
-                        icon.setBackgroundResource(R.drawable.ic_rain)
+                        weather_icon.setBackgroundResource(R.drawable.ic_rain)
                     } else if (result.weather[0].main == "Windy") {
-                        icon.setBackgroundResource(R.drawable.ic_icons8_winter_50)
+                        weather_icon.setBackgroundResource(R.drawable.ic_icons8_winter_50)
                     } else if (result.weather[0].main == "Clouds") {
-                        icon.setBackgroundResource(R.drawable.ic_clouds)
+                        weather_icon.setBackgroundResource(R.drawable.ic_clouds)
                     }
-                    sunrise.text = "Wschód słońca: ${getTime(result.sys.sunrise.toLong() * 1000)}"
-                    sunset.text = "Zachód słońca: ${getTime(result.sys.sunset.toLong() * 1000)}"
+                    weather_sunrise.text = "Wschód słońca: ${getTime(result.sys.sunrise.toLong() * 1000)}"
+                    weather_sunset.text = "Zachód słońca: ${getTime(result.sys.sunset.toLong() * 1000)}"
+
+
                 }
                 ,
-                { error -> Toast.makeText(this, "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych", Toast.LENGTH_LONG).show() } //error.message
+                { error ->
+                    Toast.makeText(
+                        this,
+                        "Wystąpił błąd. Sprawdź połączenie z internetem/poprawność danych",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } //error.message
             )
+
+        val intent = Intent(this, WeatherDetails::class.java).apply {}
+        this.startActivity(intent)
     }
+
     override fun onPause() {
         super.onPause()
         disposable?.dispose()
---
+++ app/src/main/java/com/example/kuba/ui/WeatherDetails.kt
@@ -0,0 +1,13 @@
+package com.example.kuba.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kuba.R
+
+class WeatherDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.weather_fragment)
+    }
+}
